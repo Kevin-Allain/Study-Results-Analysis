3abdc35e09b5e5a69bf5bb5dd6a2a8adec1d5259
Kevin-Allain <kevallain@gmail.com>
2021-08-22 12:24:47 +0200

    Work in progress: got a first version of a bar chart to display correctness of question B, which also could be used for the trust levels... But might need to generate a new structure that directly stores the percentages.

--- Confidence_Interval.r
+++ Confidence_Interval.r
@@ -57,49 +57,8 @@ boot_d_focus_what_qn_DiffA2_mask_easy = boot(d$diffA2[d$focus=="WHAT_Qn" & d$dMa
 boot_d_focus_what_qn_DiffA3_mask_easy = boot(d$diffA3[d$focus=="WHAT_Qn" & d$dMask=="easy"],samplemean,R=10000)
 
 
-# Used to work, now buggy
-# dfwc_new <- summarySEwithin(d, measurevar="diffA1", withinvars="dMask", idvar="idc", na.rm=FALSE, conf.interval=.95)
-# dfwc_new
-
-# ggplot(dfwc, aes(x=diffA1, y=dMask, group=1)) +
-#   # geom_line() +
-#   geom_errorbar(width=.1, aes(xmin=diffA1-ci, xmax=diffA1+ci)) +
-#   geom_point(shape=21, size=3, fill="white") +
-#   facet_wrap(~ focus)
-# #+ ylim(40,60)
 
 
-testStructure <-
-  structure(
-    list(
-      Estimate = c(0.1784, 0.073, 0.0619, 0.1367, 0.1795, 0.087),
-      name = structure( c(1L, 6L, 5L, 4L, 3L, 2L),
-        .Label = c("Intercept", "Doctor spouse", "8 years experience", "3 years experience", "1 year experience", "Female" ),
-        class = "factor"
-      ),
-      group = structure(
-        c(1L, 2L, 3L, 3L, 3L, 4L),
-        .Label = c("Intercept", "Male to", "0 Years Experience to", "No Spouse to"),
-        class = "factor"
-      ),
-      upper.95 = c(0.209, 0.0899, 0.0858, 0.1606, 0.2034, 0.1077),
-      lower.95 = c(0.1478, 0.0561, 0.038, 0.1129, 0.1556, 0.0662),
-      resp_type = c( "Legislator", "Legislator", "Legislator", "Legislator", "Legislator", "Legislator" )
-    ),
-    row.names = c(NA,-6L),
-    class = c("tbl_df", "tbl", "data.frame")
-  )
-
-testStructure
-
-# ggplot(dfwc, aes( xmin=-.10, xmax = .20, x=diffA1, y=interaction(dMask, N))) +
-#        geom_point(size = 4) +
-#        geom_errorbar(aes(x=diffA1,xmin = diffA1-ci, xmax = diffA1+ci, y=interaction(dMask, N))) #+ facet_wrap(~dMask)
-       
-# 
-# ggplot(data = d, aes(x=diffA1,y=dMask))+
-#   geom_errorbar(width=.1,aes(x=diffA1, xmin=diffA1-ci,xmax=diffA1+ci)) +
-#   geom_point(mapping = x=diffA1)#+facet_wrap(~focus)
 
 
 # TIME-global Function to find the bootstrap Confidence Intervals 
@@ -154,46 +113,60 @@ ggplot(d, aes(x=diffA1,y=focus)) +
   facet_grid(orderMaskComplex ~ orderFocusComplex)
 
 
+
+# ---- Error bars for questions A1, A2, A3 and trust levels
+
 groupedData <- d_alt %>%
   group_by(focus,dComplex_focus,dMask) %>%
-  summarize(mean_diffA1 = mean(diffA1), sd_diffA1 = sd(diffA1, na.rm=TRUE), count=n(),se_diffA1=(sd_diffA1/(sqrt(count))))
+  summarize(mean_diffA1 = mean(diffA1), sd_diffA1 = sd(diffA1, na.rm=TRUE), count=n(),se_diffA1=(sd_diffA1/(sqrt(count))),
+            mean_diffA2 = mean(diffA2), sd_diffA2 = sd(diffA2, na.rm=TRUE), count=n(),se_diffA2=(sd_diffA2/(sqrt(count))),
+            mean_diffA3 = mean(diffA3), sd_diffA3 = sd(diffA3, na.rm=TRUE), count=n(),se_diffA3=(sd_diffA3/(sqrt(count))),
+            mean_correctB = mean(correctB), sd_correctB = sd(correctB, na.rm=TRUE), count=n(),se_correctB=(sd_correctB/(sqrt(count)))
+            ) 
+# %>%
+#   mutate(percCorrectB = count/sum(count))
 
-groupedData$orderFocusComplex <- factor(groupedData$dComplex_focus,c("E","M","H"))
-groupedData$orderMaskComplex <- factor(groupedData$dMask,c("easy","medium","hard"))
 
-groupedPlot <- ggplot(groupedData, aes(x=mean_diffA1,y=focus)) +
+groupedData$dMask = as.character(groupedData$dMask)
+groupedData$dMask[groupedData$dMask == "easy"] = "Mask Easy"
+groupedData$dMask[groupedData$dMask == "medium"] = "Mask Medium"
+groupedData$dMask[groupedData$dMask == "hard"] = "Mask Hard"
+groupedData$dComplex_focus = as.character(groupedData$dComplex_focus)
+groupedData$dComplex_focus[groupedData$dComplex_focus == "E"] = "Focus Easy"
+groupedData$dComplex_focus[groupedData$dComplex_focus == "M"] = "Focus Medium"
+groupedData$dComplex_focus[groupedData$dComplex_focus == "H"] = "Focus Hard"
+
+# groupedData$orderFocusComplex <- factor(groupedData$dComplex_focus,c("E","M","H"))
+# groupedData$orderMaskComplex <- factor(groupedData$dMask,c("easy","medium","hard"))
+groupedData$orderFocusComplex <- factor(groupedData$dComplex_focus,c("Focus Easy","Focus Medium","Focus Hard"))
+groupedData$orderMaskComplex <- factor(groupedData$dMask,c("Mask Easy","Mask Medium","Mask Hard"))
+
+groupedPlotDiffA1 <- ggplot(groupedData, aes(x=mean_diffA1,y=focus)) +
   geom_errorbar(aes(xmin=mean_diffA1-se_diffA1, xmax=mean_diffA1+se_diffA1)) +
-  geom_point(size=3,col="black",fill="white") +
-  facet_wrap( ~ orderMaskComplex + orderFocusComplex  , dir="v", ncol=1) # facet_grid or facet_wrap
+  geom_point(size=3,col="black",fill="white", shape=1) +
+  facet_wrap( ~ orderMaskComplex + orderFocusComplex  , dir="v", ncol=1)
 
-d_alt[d_alt$dComplex_focus=="E" & d_alt$dMask=="easy"]
+# d_alt[d_alt$dComplex_focus=="E" & d_alt$dMask=="easy"]
 # View(groupedData)
+groupedPlotDiffA1
 
-groupedPlot  
+# ---- Stacked bar charts for question B and trust levels
 
+d_alt$dMask = as.character(d_alt$dMask)
+d_alt$dMask[d_alt$dMask == "easy"] = "Mask Easy"
+d_alt$dMask[d_alt$dMask == "medium"] = "Mask Medium"
+d_alt$dMask[d_alt$dMask == "hard"] = "Mask Hard"
+d_alt$dComplex_focus = as.character(d_alt$dComplex_focus)
+d_alt$dComplex_focus[d_alt$dComplex_focus == "E"] = "Focus Easy"
+d_alt$dComplex_focus[d_alt$dComplex_focus == "M"] = "Focus Medium"
+d_alt$dComplex_focus[d_alt$dComplex_focus == "H"] = "Focus Hard"
 
-# # AverageCorrect-global Function to find the bootstrap Confidence Intervals 
-# timeCI <- boot.ci(boot.out = bootDuration_in_seconds, type = c("norm", "basic", "perc", "bca")) 
-# lowerTimeCI <- timeCI$normal[2];higherTimeCI <- timeCI$normal[3]
-# vecTimeCI <- c(lowerTimeCI, higherTimeCI)
-# 
-# #Buggy, don't remember why. Disregard.
-# bootDuration_in_secondsEEE = boot(d$t[d$ComplexityGroup=="EEE"], samplemean, R=1000) # 1000 replications
-# bootDuration_in_secondsMMM = boot(d$t[d$ComplexityGroup=="MMM"], samplemean, R=1000) # 1000 replications
-# bootDuration_in_secondsHHH = boot(d$t[d$ComplexityGroup=="HHH"], samplemean, R=1000) # 1000 replications
-# timeCIEEE <- boot.ci(boot.out = bootDuration_in_secondsEEE, type = c("norm", "basic", "perc", "bca"))
-# timeCIMMM <- boot.ci(boot.out = bootDuration_in_secondsMMM, type = c("norm", "basic", "perc", "bca"))
-# timeCIHHH <- boot.ci(boot.out = bootDuration_in_secondsHHH, type = c("norm", "basic", "perc", "bca"))
-# indexesV <- 1:length(c(timeCIEEE$normal[2],timeCIEEE$normal[3]))
-# indexesCategories <- 1:3
-# # Is that the right means?
-# meansCategories <- c(mean(d$t[d$ComplexityGroup=="EEE"]), mean(d$t[d$ComplexityGroup=="MMM"]), mean(d$t[d$ComplexityGroup=="HHH"]))
-# 
-# dfCategories <- data.frame(x= c("EEE","MMM","HHH") ,
-#                            F = meansCategories, L = c(timeCIEEE$normal[2], timeCIMMM$normal[2], timeCIHHH$normal[2]),
-#                            U = c(timeCIEEE$normal[3], timeCIMMM$normal[3], timeCIHHH$normal[3])
-# )
-# ggplot(dfCategories, aes(x = x, y = F)) + geom_point(size = 4) + geom_errorbar(aes(ymax = U, ymin = L))
- 
+d_alt$orderFocusComplex <- factor(d_alt$dComplex_focus,c("Focus Easy","Focus Medium","Focus Hard"))
+d_alt$orderMaskComplex <- factor(d_alt$dMask,c("Mask Easy","Mask Medium","Mask Hard"))
+groupedPlotCorrectB <- ggplot(d_alt, aes(x=(..count../sum(..count..)), y=focus)) +
+  geom_bar(aes(fill=factor(correctB)),position=position_stack(reverse=TRUE)) +
+  theme(legend.position = "top") +
+  facet_wrap( ~ orderMaskComplex + orderFocusComplex  , dir="v", ncol=1)
 
+groupedPlotCorrectB
 
